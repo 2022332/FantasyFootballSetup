03d2c14843c8fac1d2c0703609c2b461aa7df125
2022332 <2022332@student.cct.ie>
2023-09-27 14:34:48 +0100

    Striker

---
+++ src/fantasy/football/setup/Stricker.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fantasy.football.setup;
+
+/**
+ *
+ * @author Ari
+ */
+public class Stricker extends Player{
+    
+    public Stricker(String fname, String lname, String country, int age, String position) {
+        super(fname, lname, country, age, position);
+    }
+    
+}
